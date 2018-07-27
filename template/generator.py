92e23b7622e565f6347833a26c8c6bfb94dc2103
Flex Ferrum <flexferrum@gmail.com>
2018-07-28 01:39:39 +0300

    Add Jinja2-based python generator for variant implementation

---
+++ template/generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from jinja2 import Template, Environment, FileSystemLoader
+
+def ProcessTemplate(src, dst):
+    loader = FileSystemLoader('.')
+    env = Environment(loader=loader)
+
+    tpl = env.get_template(src)
+    result = tpl.render(NumParams=16)
+#    print (result)
+    fout = open(dst, 'wt')
+    fout.write(result)
+    fout.close()
+
+def main():
+    ProcessTemplate('./variant.hpp', '../include/nonstd/variant.hpp')
+
+if __name__== "__main__":
+  main()
