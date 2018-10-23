0136cae37f3715f0544169adde0b8496a06f49fb
Martin Moene <martin.moene@gmail.com>
2018-10-23 23:02:16 +0200

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile
+
+class VariantLiteConan(ConanFile):
+    version = "0.0.0"
+    name = "variant-lite"
+    description = "A single-file header-only version of a C++17-like variant, a type-safe union for C++98, C++11 and later"
+    license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
+    url = "https://github.com/martinmoene/variant-lite"
+    exports_sources = "include/nonstd/*", "LICENSE.txt"
+    author = "Martin Moene"
+
+    def package_info(self):
+        self.info.header_only()
