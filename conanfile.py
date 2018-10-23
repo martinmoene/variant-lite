91cf10c2989efd02ec6da26ac665fe01d3d5c41f
Martin Moene <martin.moene@gmail.com>
2018-10-24 00:23:05 +0200

    Correct url

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class VariantLiteConan(ConanFile):
     name = "variant-lite"
     description = "A single-file header-only version of a C++17-like variant, a type-safe union for C++98, C++11 and later"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
-    url = "https://github.com/martinmoene/variant-lite"
+    url = "https://github.com/martinmoene/variant-lite.git"
     exports_sources = "include/nonstd/*", "LICENSE.txt"
     author = "Martin Moene"
 
